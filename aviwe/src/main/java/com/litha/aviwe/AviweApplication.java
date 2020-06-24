752da61691e9ec2678518bcd2d5b45ef43ba54a3
Admin <Admin@192.168.0.101>
2020-06-24 13:57:46 +0200

    initial commit of project

---
+++ aviwe/src/main/java/com/litha/aviwe/AviweApplication.java
@@ -0,0 +1,13 @@
+package com.litha.aviwe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AviweApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AviweApplication.class, args);
+	}
+
+}
---
+++ aviwe/src/test/java/com/litha/aviwe/AviweApplicationTests.java
@@ -0,0 +1,13 @@
+package com.litha.aviwe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AviweApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
